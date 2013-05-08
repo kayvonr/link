b3be79f5e12f67a85215d5d7a31037e8af8d0822
Adam Donahue <adam@appnexus.com>
2013-05-08 01:59:57 -0400

    Some clean-up.

--- link/utils.py
+++ link/utils.py
@@ -1,4 +1,8 @@
+import json
 import os
+
+from itertools import izip, chain, repeat
+
 # -*- coding: utf-8 -*-
 
 """
@@ -13,26 +17,16 @@ Commonly used utility functions in the link code
 """
 
 def load_json_file(file_name):
-    """
-    given a file name this function will json decode it and
-    return a dictionary
-    """
-    try:
-        data = open(file_name, 'r').read()
-    except:
-        raise Exception("No such file %s" % file_name)
+    """Given a file name this function will json decode it and
+    return a dictionary.
 
-    import json
-    try:
-        return json.loads(data)
-    except Exception as e:
-        raise ValueError("Error json decoding file: %s error: %s" % (file_name,
-                                                                     e.message))
+    """
+    return json.load(file(file_name))
 
 def list_to_dataframe(rows, names):
     """
-    Turns a rows of data into a dataframe and gives them the column names
-    specified
+    Turns an array of rows of data into a dataframe and gives them the column names
+    specified.
 
     :params rows: the data you want to put in the dataframe
     :params names: the column names for the dataframe
@@ -48,17 +42,19 @@ def list_to_dataframe(rows, names):
 
     columns = dict(zip(names, lib.to_object_array_tuples(rows).T))
 
-    for k, v in columns.iteritems():
+    for k,v in columns.iteritems():
         columns[k] = lib.convert_sql_column(v)
 
     return DataFrame(columns, columns=names)
 
-
-from itertools import izip, chain, repeat
-
 def array_pagenate(n, iterable, padvalue=None):
-    """
-    takes an array like [1,2,3,4,5] and splits it into even chunks.  It will
-    pad the end with your default value to make fully even.  
+    """Takes an array like [1,2,3,4,5] and splits it into evenly-sized
+    chunks.
+
+    The last chunk is padded with the specified padvalue to
+    ensure its length equals that of the other chunks.
+
     """
     return izip(*[chain(iterable, repeat(padvalue, n-1))]*n)
+
+array_paginate = array_pagenate
